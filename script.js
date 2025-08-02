1c438eb844f6cb0538f2f8ec5070e89a129b68fb
xxaianadisousa <xailuciana@gmail.com>
2025-08-02 02:02:01 -0300

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let tempo = 300;
-const cronometro = document.getElementById("cronometro
+function destacarDica(el) {
+  el.style.backgroundColor = "#d0f0ff";
+}
+
+function reiniciarJogo() {
+  document.querySelectorAll('.casa input').forEach(input => {
+    input.value = "";
+    input.style.borderColor = "#aaa";
+  });
+}
+
+function mostrarDicaAleatoria() {
+  const dicas = document.querySelectorAll('.dicas p');
+  const aleatoria = dicas[Math.floor(Math.random() * dicas.length)];
+  alert("💡 Dica aleatória:\n\n" + aleatoria.textContent);
+}
